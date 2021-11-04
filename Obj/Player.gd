fa7ce12f3575a321d381aa4dfd1055fe0b2de9fb
LJChao3473 <ljchao3473@gmai.com>
2021-11-04 15:33:53 +0100

    double jump tests

--- Obj/Player.gd
+++ Obj/Player.gd
@@ -2,8 +2,9 @@ extends KinematicBody2D
 
 
 var speed = 200
-var jump = 600
+var jump = 400
 var gravity = 800
+var jumpCount = 1
 
 var vel : Vector2 = Vector2()
 
@@ -12,15 +13,25 @@ onready var sprite : Sprite = get_node("Sprite")
 func _physics_process(delta):
 	vel.x = 0
 	vel.y += gravity*delta
-	
+
+	#Move RL
 	if Input.is_action_pressed("Left_WASD"):
 		vel.x -= speed
 	if Input.is_action_pressed("Right_WASD"):
 		vel.x += speed
-	#if Input.is_action_pressed("Down_WASD"):
-	#	vel.y += speed
-	if Input.is_action_pressed("Up_WASD") and is_on_floor():
-		vel.y -= jump
+
+	if is_on_floor():
+		jumpCount = 1
+
+	#Jump + Double Jump
+	if Input.is_action_just_pressed("Up_WASD"):
+		if is_on_floor():
+			vel.y -= jump
+		elif jumpCount == 1:
+			vel.y -= jump
+			jumpCount = 0
+	
+		
 	if Input.is_action_pressed("Down_WASD") and !is_on_floor():
 		vel.y += speed
 	
